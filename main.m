fe0d0e8d550cc787ae12e257f29c2c5ef56612fe
Kacper Wach <wach.kacper8@gmail.com>
2025-04-21 13:09:06 +0200

    Dodatkowe zmiany

--- main.m
+++ main.m
@@ -1,14 +1,22 @@
 clear all
 clc
 
-T = 2;
-P = 1.25;
-I = 1/4;
-D = 0.8;
+T = 10;
+P = 1.825;
+I = 1/5;
+D = 0.9;
+% kr = 12.549;
+% Ti = 1.576;
+% Td = 0.394;
+% 
+% P = kr;
+% I = 1/Ti;
+% D = kr*Td;
 
 out = sim("Obiekt123.slx");
 e = out.e.Data;
 y = out.y.Data;
+u = out.u.Data;
 t = out.tout;
 figure
 plot(t,y);
@@ -20,4 +28,18 @@ plot(t,e);
 xlabel('Czas, s')
 ylabel('Uchyb')
 
-ISE = sum(e.^2);
+figure
+plot(t,u);
+xlabel('Czas, s')
+ylabel('Sygnał sterowania')
+
+
+ISE = sum(e.^2)
+ICE = sum(u.^2);
+
+% org = out.oryginalny.Data;
+% figure
+% plot(t,org);
+% xlabel('Czas, s')
+% ylabel('Sygnał oryginalny przefiltrowany')
+
